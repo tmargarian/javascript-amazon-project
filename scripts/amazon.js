f9281492a29d64d3a1d232444297d129079c24e5
Tigran Margarian <tig.margarian@gmail.com>
2023-12-10 14:10:03 -0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,8 +75,16 @@ document.querySelectorAll('.js-add-to-cart')
         cart.push({
           productId: productId,
           quantity: 1
-      });
+        });
       }
-      console.log(cart);
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+
+      document.querySelector('.js-cart-quantity')
+        .textContent = `${cartQuantity}`;
     });
   });
